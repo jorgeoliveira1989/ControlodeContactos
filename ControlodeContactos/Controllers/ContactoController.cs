fb6bd9e56a1736f743131667ad33a17b4ed783cc
Jorge Oliveira <jorgemigueloliveira1989@gmail.com>
2024-03-26 23:34:22 +0000

    Criação do Layout para o CRUD!!

---
+++ ControlodeContactos/Controllers/ContactoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControlodeContactos.Controllers
+{
+    public class ContactoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+    }
+}
