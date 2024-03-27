63d22a84fe26c6fd80550acc36375205a9271b02
Jorge Oliveira <jorgemigueloliveira1989@gmail.com>
2024-03-27 19:05:50 +0000

    Criação da Ligação ao SQL SERVER

---
+++ ControlodeContactos/Data/BDContext.cs
@@ -0,0 +1,14 @@
+﻿using ControlodeContactos.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControlodeContactos.Data
+{
+    public class BDContext : DbContext
+    {
+        public BDContext(DbContextOptions<BDContext> options) : base(options)
+        {
+        }
+        public DbSet<ContactoModel> Contactos { get; set; }
+    }
+}
--- ControlodeContactos/Program.cs
+++ ControlodeContactos/Program.cs
@@ -1,7 +1,14 @@
+using ControlodeContactos.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<BDContext>(Options =>
+{
+    Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
